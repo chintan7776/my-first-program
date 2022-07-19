887131245c7607d466dfef0c7c869f35832ad47e
Chintan Patel <Chintanpatel7776@gmail.com>
2022-07-19 20:55:14 +0100

    my first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+package JavaConcepts;
+
+public class MyFirstProgram
+{
+    public static void main(String[] args) {
+        System.out.println("My First Application");
+                System.out.println("Tester Test");
+    }
+
+}
