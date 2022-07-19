5397d0598ec231aa0a80e9a63c8dd42cfb7d0d94
Chintan Patel <Chintanpatel7776@gmail.com>
2022-07-19 21:07:09 +0100

    message change

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram
 {
     public static void main(String[] args) {
         System.out.println("My First Application");
-                System.out.println("Tester Test");
+                System.out.println("Chintan");
     }
 
 }
